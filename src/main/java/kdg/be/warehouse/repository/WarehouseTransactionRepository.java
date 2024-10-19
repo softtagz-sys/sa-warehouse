4159e365904dfaaff03f1c6a8f27d1418eb12f7f
Kobe Ponet <kobe.ponet@student.kdg.be>
2024-10-19 12:23:05 +0000

    Fulfill po

---
+++ src/main/java/kdg/be/warehouse/repository/WarehouseTransactionRepository.java
@@ -0,0 +1,15 @@
+package kdg.be.warehouse.repository;
+
+import kdg.be.warehouse.domain.warehouse.Warehouse;
+import kdg.be.warehouse.domain.warehouse.WarehouseTransaction;
+import kdg.be.warehouse.domain.material.Material;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.UUID;
+
+@Repository
+public interface WarehouseTransactionRepository extends JpaRepository<WarehouseTransaction, UUID> {
+    List<WarehouseTransaction> findByWarehouseAndMaterialOrderByTransactionTimeAsc(Warehouse warehouse, Material material);
+}
